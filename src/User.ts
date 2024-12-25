2f1ff6b1b42f8b59c248aaf9a0ced9b7c80b3179
Mrjuanblack <juancho_2ce@hotmail.com>
2024-12-25 15:12:16 -0500

    Updated user model

--- src/User.ts
+++ src/User.ts
@@ -1,9 +1,10 @@
 export interface User {
-    id: string
+    id: number
     name: string
     email: string
+    phoneNumber: string
 }
 
 export class UserEntity implements User {
-    constructor(public id: string, public name: string, public email: string) {}
+    constructor(public id: number, public name: string, public email: string, public phoneNumber: string) {}
 }
