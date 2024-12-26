439cfc767d0121f0fe57dd1eae5881e7e9c48305
Mrjuanblack <juancho_2ce@hotmail.com>
2024-12-26 18:29:38 -0500

    Separated Ok and Error type

--- dist/RequestResult.d.ts
+++ dist/RequestResult.d.ts
@@ -1,10 +1,12 @@
-export type RequestResult<T> = {
+export type OkResult<T> = {
     ok: true;
     value: T;
-} | {
+};
+export type ErrorResult = {
     ok: false;
     message: string;
     code: string;
 };
+export type RequestResult<T> = OkResult<T> | ErrorResult;
 export declare function requestOk<T>(value: T): RequestResult<T>;
 export declare function requestError<T>(message: string, code: string): RequestResult<T>;
--- src/RequestResult.ts
+++ src/RequestResult.ts
@@ -1,11 +1,15 @@
-export type RequestResult<T> = {
+export type OkResult<T> = {
     ok: true;
     value: T;
-} | {
+}
+
+export type ErrorResult = {
     ok: false;
     message: string;
     code: string;
 }
+
+export type RequestResult<T> = OkResult<T> | ErrorResult
 
 export function requestOk<T>(value:T): RequestResult<T> {
     return { ok: true, value }
