a3471f813d750716dd55e026183c590ed3278b4d
Mrjuanblack <juancho_2ce@hotmail.com>
2024-12-25 19:16:07 -0500

    Added error type for user display

---
+++ dist/Error.d.ts
@@ -0,0 +1,8 @@
+export declare enum RepositoryUser {
+    GetAll = "U_001",
+    Get = "U_002",
+    Update = "U_003",
+    UpdateMany = "U_004",
+    Remove = "U_005",
+    RemoveMany = "U_006"
+}
---
+++ dist/Error.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RepositoryUser = void 0;
+var RepositoryUser;
+(function (RepositoryUser) {
+    RepositoryUser["GetAll"] = "U_001";
+    RepositoryUser["Get"] = "U_002";
+    RepositoryUser["Update"] = "U_003";
+    RepositoryUser["UpdateMany"] = "U_004";
+    RepositoryUser["Remove"] = "U_005";
+    RepositoryUser["RemoveMany"] = "U_006";
+})(RepositoryUser || (exports.RepositoryUser = RepositoryUser = {}));
