ee384c800709254060930a283b0182e8cba9f668
Mrjuanblack <juancho_2ce@hotmail.com>
2025-03-04 15:51:49 -0500

    Added websockets results

--- dist/WebsocketsConstants.d.ts
+++ dist/WebsocketsConstants.d.ts
@@ -2,3 +2,12 @@ export declare enum WS_Emails {
     EmailProgress = "emailProgress",
     EmailError = "emailError"
 }
+export interface EmailProgressResult {
+    index: number;
+    total: number;
+}
+export interface EmailError {
+    index: number;
+    total: number;
+    error: string;
+}
--- src/WebsocketsConstants.ts
+++ src/WebsocketsConstants.ts
@@ -2,3 +2,14 @@ export enum WS_Emails {
     EmailProgress = "emailProgress",
     EmailError = "emailError"
 }
+
+export interface EmailProgressResult {
+    index: number
+    total: number
+}
+
+export interface EmailError {
+    index: number
+    total: number
+    error: string
+}
