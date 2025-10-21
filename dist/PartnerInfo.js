49af49c2054457611a07251f53a2e4e8af43976b
Neider08Urbano <jjuly8julianur@gmail.com>
2025-10-21 15:41:08 -0500

    ajuste projecto sanupa domain

--- dist/PartnerInfo.js
+++ dist/PartnerInfo.js
@@ -2,27 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SimplePartnerInfoEntity = exports.PartnerInfoEntity = void 0;
 class PartnerInfoEntity {
-    constructor(partnerId, name, zone, address, workingArea_Zones, workingArea_Communities, workingArea_Cities, deliveryWorking_Zones, deliveryWorking_Communities, deliveryWorking_Cities, imgPath) {
-        this.partnerId = partnerId;
-        this.name = name;
+    constructor(zone, name, address, partnerId, imgPath, workingArea_Zones, workingArea_Regions, workingArea_Cities, deliveryWorking_Zones, deliveryWorking_Cities, workingArea_Communities, deliveryWorking_Communities) {
         this.zone = zone;
+        this.name = name;
         this.address = address;
+        this.partnerId = partnerId;
+        this.imgPath = imgPath;
         this.workingArea_Zones = workingArea_Zones;
-        this.workingArea_Communities = workingArea_Communities;
+        this.workingArea_Regions = workingArea_Regions;
         this.workingArea_Cities = workingArea_Cities;
         this.deliveryWorking_Zones = deliveryWorking_Zones;
-        this.deliveryWorking_Communities = deliveryWorking_Communities;
         this.deliveryWorking_Cities = deliveryWorking_Cities;
-        this.imgPath = imgPath;
+        this.workingArea_Communities = workingArea_Communities;
+        this.deliveryWorking_Communities = deliveryWorking_Communities;
     }
 }
 exports.PartnerInfoEntity = PartnerInfoEntity;
 class SimplePartnerInfoEntity {
-    constructor(partnerId, name, address, zone, imgPath) {
-        this.partnerId = partnerId;
+    constructor(zone, name, address, partnerId, imgPath) {
+        this.zone = zone;
         this.name = name;
         this.address = address;
-        this.zone = zone;
+        this.partnerId = partnerId;
         this.imgPath = imgPath;
     }
 }
--- dist/PartnerWorkingZone.js
+++ dist/PartnerWorkingZone.js
@@ -1,48 +1,83 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.NewPartnerWorking_CityEntity = exports.PartnerWorking_CityEntity = exports.NewPartnerWorking_CommunityEntity = exports.PartnerWorking_CommunityEntity = exports.NewPartnerWorking_ZoneEntity = exports.PartnerWorking_ZoneEntity = void 0;
+exports.NewPartnerWorking_RegionEntity = exports.PartnerWorking_RegionEntity = exports.NewPartnerWorking_CityEntity = exports.PartnerWorking_CityEntity = exports.NewPartnerWorking_CommunityEntity = exports.PartnerWorking_CommunityEntity = exports.NewPartnerWorking_ZoneEntity = exports.PartnerWorking_ZoneEntity = void 0;
 class PartnerWorking_ZoneEntity {
-    constructor(zone, price) {
-        this.zone = zone;
+    constructor(price, zoneId, leadTime, partnerId, days) {
         this.price = price;
+        this.zoneId = zoneId;
+        this.leadTime = leadTime;
+        this.partnerId = partnerId;
+        this.days = days;
     }
 }
 exports.PartnerWorking_ZoneEntity = PartnerWorking_ZoneEntity;
 class NewPartnerWorking_ZoneEntity {
-    constructor(partnerId, zoneId, price) {
-        this.partnerId = partnerId;
-        this.zoneId = zoneId;
+    constructor(price, zoneId, leadTime, partnerId, days) {
         this.price = price;
+        this.zoneId = zoneId;
+        this.leadTime = leadTime;
+        this.partnerId = partnerId;
+        this.days = days;
     }
 }
 exports.NewPartnerWorking_ZoneEntity = NewPartnerWorking_ZoneEntity;
 class PartnerWorking_CommunityEntity {
-    constructor(community, price) {
-        this.community = community;
+    constructor(price, leadTime, partnerId, communityId, days) {
         this.price = price;
+        this.leadTime = leadTime;
+        this.partnerId = partnerId;
+        this.communityId = communityId;
+        this.days = days;
     }
 }
 exports.PartnerWorking_CommunityEntity = PartnerWorking_CommunityEntity;
 class NewPartnerWorking_CommunityEntity {
-    constructor(partnerId, communityId, price) {
+    constructor(price, leadTime, partnerId, communityId, days) {
+        this.price = price;
+        this.leadTime = leadTime;
         this.partnerId = partnerId;
         this.communityId = communityId;
-        this.price = price;
+        this.days = days;
     }
 }
 exports.NewPartnerWorking_CommunityEntity = NewPartnerWorking_CommunityEntity;
 class PartnerWorking_CityEntity {
-    constructor(city, price) {
-        this.city = city;
+    constructor(price, cityId, leadTime, partnerId, days) {
         this.price = price;
+        this.cityId = cityId;
+        this.leadTime = leadTime;
+        this.partnerId = partnerId;
+        this.days = days;
     }
 }
 exports.PartnerWorking_CityEntity = PartnerWorking_CityEntity;
 class NewPartnerWorking_CityEntity {
-    constructor(partnerId, cityId, price) {
-        this.partnerId = partnerId;
-        this.cityId = cityId;
+    constructor(price, cityId, leadTime, partnerId, days) {
         this.price = price;
+        this.cityId = cityId;
+        this.leadTime = leadTime;
+        this.partnerId = partnerId;
+        this.days = days;
     }
 }
 exports.NewPartnerWorking_CityEntity = NewPartnerWorking_CityEntity;
+class PartnerWorking_RegionEntity {
+    constructor(price, regionId, leadTime, partnerId, days) {
+        this.price = price;
+        this.regionId = regionId;
+        this.leadTime = leadTime;
+        this.partnerId = partnerId;
+        this.days = days;
+    }
+}
+exports.PartnerWorking_RegionEntity = PartnerWorking_RegionEntity;
+class NewPartnerWorking_RegionEntity {
+    constructor(price, regionId, leadTime, partnerId, days) {
+        this.price = price;
+        this.regionId = regionId;
+        this.leadTime = leadTime;
+        this.partnerId = partnerId;
+        this.days = days;
+    }
+}
+exports.NewPartnerWorking_RegionEntity = NewPartnerWorking_RegionEntity;
